019f6079b9befe96d4260c7575195081f0baf529
jeonzi <jieun5230@naver.com>
2019-03-21 19:32:02 +0900

    front-end

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ const app = express(); // node express로 서버 생성하기
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // allow everyone, everywhere connect localhost server
+  res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(isAuth);
 
 app.use(
@@ -30,7 +40,7 @@ mongoose
     }@run2019-lcao6.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`
   )
   .then(() => {
-    app.listen(3000);
+    app.listen(8000);
   })
   .catch(err => {
     console.log(err);
